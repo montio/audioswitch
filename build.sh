49b2144b2b71e7371d0324f1330d64d4477b2ee6
Ralf Stemmer <ralf.stemmer@gmx.net>
2013-01-29 14:41:27 +0100

    readme-updates

--- build.sh
+++ build.sh
@@ -8,9 +8,3 @@ cd ..
 #plasmapkg -i "pulse control.plasmoid"
 
 plasmoidviewer "$NAME"
-
-
-# zip -r ../hello-python.zip .
-
-# To uninstall our applet we use plasmapkg again with its -r option.
-# plasmapkg -r hello-python
